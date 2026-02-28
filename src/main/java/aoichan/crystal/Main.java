d3827af69558b9b1054275b4ce4a862cb50c78c0
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-02-28 23:23:35 +0700

    Update Main.java

--- src/main/java/aoichan/crystal/Main.java
+++ src/main/java/aoichan/crystal/Main.java
@@ -1,112 +1,80 @@
 package aoidev.crystal;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import aoidev.crystal.bootstrap.ConsoleBanner;
+import aoidev.crystal.gem.GemManager;
+import aoidev.crystal.gem.GemRegistry;
+import aoidev.crystal.hook.PlaceholderHook;
+import aoidev.crystal.hook.VaultHook;
+import aoidev.crystal.storage.HikariProvider;
+import aoidev.crystal.storage.SQLiteStorage;
+import aoidev.crystal.storage.StorageAdapter;
+import aoidev.crystal.ui.UISystem;
+import aoidev.crystal.security.AntiDuper;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Level;
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public final class Main extends JavaPlugin {
 
-    private static Main instance;
-    private Gson gson;
+    private StorageAdapter storage;
+    private HikariProvider hikari;
+    private ExecutorService dbExecutor;
+
     private GemManager gemManager;
-    private StorageManager storageManager;
-    private SocketServer socketServer;
-    private UISystem uiSystem;
-    private AdminCommands adminCommands;
-    private AntiDuper antiDuper;
-
-    public static Main getInstance() {
-        return instance;
-    }
-
-    @Override
-    public void onLoad() {
-        instance = this;
-        saveDefaultConfig();
-    }
+    private GemRegistry registry;
+    private VaultHook vaultHook;
 
     @Override
     public void onEnable() {
-        this.gson = new GsonBuilder().setPrettyPrinting().create();
 
-        // Initialize core subsystems
-        this.storageManager = new StorageManager(this, gson);
-        this.gemManager = new GemManager(this, storageManager);
-        this.uiSystem = new UISystem(this, gemManager);
-        this.antiDuper = new AntiDuper(getConfig().getLong("anti_dupe.recent_window_ms", 2000));
-        this.adminCommands = new AdminCommands(this, gemManager, storageManager);
+        saveDefaultConfig();
+        getDataFolder().mkdirs();
 
-        // Socket server (optional)
-        if (getConfig().getBoolean("socket.enabled", true)) {
-            int port = getConfig().getInt("socket.port", 31337);
-            this.socketServer = new SocketServer(this, port, gemManager);
-            socketServer.start();
+        // Dedicated DB thread pool
+        dbExecutor = Executors.newFixedThreadPool(4);
+
+        // Hikari
+        hikari = new HikariProvider(
+                getDataFolder(),
+                getConfig().getString("storage.sqlite-file", "gems.db")
+        );
+
+        storage = new SQLiteStorage(hikari.get(), dbExecutor);
+
+        registry = new GemRegistry();
+        gemManager = new GemManager(this, storage, dbExecutor);
+
+        // Vault Hook
+        if (getServer().getPluginManager().getPlugin("Vault") != null) {
+            vaultHook = new VaultHook();
+            if (vaultHook.hook()) {
+                getLogger().info("Vault hooked successfully.");
+            }
         }
 
-        // Register command(s)
-        getCommand("gems").setExecutor(adminCommands);
-        getCommand("gems").setTabCompleter(adminCommands);
+        // PlaceholderAPI Hook
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new PlaceholderHook(gemManager).register();
+            getLogger().info("PlaceholderAPI hooked successfully.");
+        }
 
-        // Register listeners
-        getServer().getPluginManager().registerEvents(new CoreListeners(this, gemManager, antiDuper), this);
+        // Load gems
+        gemManager.loadAll();
 
-        // Load existing gems async
-        storageManager.loadAllAsync().exceptionally(ex -> {
-            getLogger().log(Level.SEVERE, "Failed loading gems", ex);
-            return null;
-        });
-
-        printConsoleBanner();
-        getLogger().info("Gems Ultimate enabled. Ready for action!");
+        ConsoleBanner.print(this);
+        getLogger().info("Gems Ultimate fully initialized.");
     }
 
     @Override
     public void onDisable() {
-        if (socketServer != null) socketServer.shutdown();
-        storageManager.shutdown();
-        getLogger().info("Gems Ultimate disabled.");
-    }
 
-    private void printConsoleBanner() {
-        String banner =
-                "\n\n" +
-                "  ____                     _                 _   _ _ _       _   _ _ _       \n" +
-                " / ___|_ __ __ _ _ __ ___ | |__   ___   ___ | |_(_) (_) ___ | |_(_) |_ ___ \n" +
-                "| |  _| '__/ _` | '_ ` _ \\| '_ \\ / _ \\ / _ \\| __| | | |/ _ \\| __| | __/ _ \\\n" +
-                "| |_| | | | (_| | | | | | | |_) | (_) | (_) | |_| | | | (_) | |_| | ||  __/\n" +
-                " \\____|_|  \\__,_|_| |_| |_|_.__/ \\___/ \\___/ \\__|_|_|_|\\___/ \\__|_|\\__\\___|\n" +
-                "\n";
-        getLogger().info(banner);
-        getLogger().info("Gems Ultimate v" + getDescription().getVersion());
-        getLogger().info("Authors: " + String.join(", ", getDescription().getAuthors()));
-        getLogger().info("Server: " + Bukkit.getName() + " " + Bukkit.getBukkitVersion());
-        Runtime rt = Runtime.getRuntime();
-        long mb = 1024 * 1024;
-        getLogger().info(String.format("Memory: free=%dMB total=%dMB max=%dMB",
-                rt.freeMemory() / mb, rt.totalMemory() / mb, rt.maxMemory() / mb));
-        getLogger().info("DataFolder: " + getDataFolder().getAbsolutePath());
-    }
+        if (storage != null) storage.shutdown();
+        if (hikari != null) hikari.shutdown();
+        if (dbExecutor != null) dbExecutor.shutdownNow();
 
-    public Gson getGson() {
-        return gson;
+        getLogger().info("Gems Ultimate shutdown complete.");
     }
-
-    public GemManager getGemManager() {
-        return gemManager;
-    }
-
-    public StorageManager getStorageManager() {
-        return storageManager;
-    }
-
-    public UISystem getUiSystem() {
-        return uiSystem;
-    }
-
-    public AntiDuper getAntiDuper() {
-        return antiDuper;
-    }
-  }
+}
