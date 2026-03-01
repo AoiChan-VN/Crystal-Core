2711fb1eea3cb53a56072f1b4fa224f61feb6fa4
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-02 02:40:55 +0700

    Update ConsoleBanner.java

--- src/main/java/aoichan/crystal/core/ConsoleBanner.java
+++ src/main/java/aoichan/crystal/core/ConsoleBanner.java
@@ -3,13 +3,11 @@ package aoichan.crystal.core;
 import org.bukkit.plugin.Plugin;
 
 public class ConsoleBanner {
-
     public static void show(Plugin plugin) {
-
         plugin.getLogger().info("====================================");
-        plugin.getLogger().info("   Gems Ultimate Production Mode   ");
+        plugin.getLogger().info("   Plugin: Crystal💎 Ultimate   ");
         plugin.getLogger().info("   Version: " + plugin.getDescription().getVersion());
-        plugin.getLogger().info("   Author: AoiDev");
+        plugin.getLogger().info("   Author: AoiChan❤");
         plugin.getLogger().info("====================================");
     }
 }
