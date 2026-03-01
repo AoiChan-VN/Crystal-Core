711864bc145118b9df8d411c87b674d97c900936
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-01 19:01:20 +0700

    Update AoiMain.java

--- src/main/java/aoichan/crystal/AoiMain.java
+++ src/main/java/aoichan/crystal/AoiMain.java
@@ -1,6 +1,8 @@
 package aoichan.crystal;
 
+import aoichan.crystal.api.GemsAPI;
 import aoichan.crystal.core.*;
+import aoichan.crystal.gui.GUIListener;
 import aoichan.crystal.storage.*;
 import aoichan.crystal.commands.GemsCommand;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,8 +10,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class AoiMain extends JavaPlugin {
 
     private static AoiMain instance;
+
     private StorageProvider storage;
     private GemsManager gemsManager;
+    private SocketManager socketManager;
+    private CacheManager cacheManager;
+    private GemsAPI api;
 
     public static AoiMain get() {
         return instance;
@@ -22,36 +28,43 @@ public final class AoiMain extends JavaPlugin {
         saveDefaultConfig();
         saveResource("gems.yml", false);
 
-        ConsoleBanner.show(this);
+        if (getConfig().getBoolean("banner.enabled"))
+            ConsoleBanner.show(this);
 
         setupStorage();
-        this.gemsManager = new GemsManager(this);
+
+        cacheManager = new CacheManager();
+        gemsManager = new GemsManager(this);
+        socketManager = new SocketManager(this);
+        api = new GemsAPI(gemsManager, socketManager);
 
         getCommand("gems").setExecutor(new GemsCommand());
 
-        getServer().getPluginManager().registerEvents(new AntiDupeManager(), this);
+        getServer().getPluginManager().registerEvents(
+                new AntiDupeManager(socketManager), this);
+        getServer().getPluginManager().registerEvents(
+                new GUIListener(), this);
 
-        getLogger().info("GemsUltimate loaded successfully.");
+        getLogger().info("GemsUltimate Production loaded.");
     }
 
     private void setupStorage() {
         String type = getConfig().getString("storage.type");
 
-        if ("MYSQL".equalsIgnoreCase(type)) {
+        if ("MYSQL".equalsIgnoreCase(type))
             storage = new MySQLStorage(this);
-        } else {
+        else
             storage = new SQLiteStorage(this);
-        }
 
         storage.connect();
     }
 
-    public StorageProvider getStorage() {
-        return storage;
+    public GemsAPI getAPI() {
+        return api;
     }
 
-    public GemsManager getGemsManager() {
-        return gemsManager;
+    public StorageProvider getStorage() {
+        return storage;
     }
 
     @Override
