3e1c992d638cc06e90b36bc817bfbf0531cba2d7
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-02 02:34:03 +0700

    Update GemData.java

--- src/main/java/aoichan/crystal/api/GemData.java
+++ src/main/java/aoichan/crystal/api/GemData.java
@@ -1,1 +1,14 @@
+package aoichan.crystal.api;
 
+public class GemData {
+    private final String id;
+    private final String displayName;
+
+    public GemData(String id, String displayName) {
+        this.id = id;
+        this.displayName = displayName;
+    }
+
+    public String getId() { return id; }
+    public String getDisplayName() { return displayName; }
+}
