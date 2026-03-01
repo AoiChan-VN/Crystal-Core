49c664d3376080fd99d41c0c4b6f927197215de4
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-02 01:32:26 +0700

    Update DatabasePool.java

--- src/main/java/aoichan/crystal/storage/DatabasePool.java
+++ src/main/java/aoichan/crystal/storage/DatabasePool.java
@@ -9,30 +9,28 @@ public class DatabasePool {
     private final HikariDataSource dataSource;
 
     public DatabasePool(AoiMain plugin) {
-
         HikariConfig config = new HikariConfig();
 
-        String type = plugin.getConfig().getString("storage.type");
+        String type = plugin.getConfig().getString("storage.type", "SQLITE");
 
         if ("MYSQL".equalsIgnoreCase(type)) {
-            String host = plugin.getConfig().getString("mysql.host");
-            String port = plugin.getConfig().getString("mysql.port");
-            String db = plugin.getConfig().getString("mysql.database");
-            String user = plugin.getConfig().getString("mysql.username");
-            String pass = plugin.getConfig().getString("mysql.password");
+            String host = plugin.getConfig().getString("mysql.host", "localhost");
+            String port = plugin.getConfig().getString("mysql.port", "3306");
+            String db = plugin.getConfig().getString("mysql.database", "gems");
+            String user = plugin.getConfig().getString("mysql.username", "root");
+            String pass = plugin.getConfig().getString("mysql.password", "");
 
-            config.setJdbcUrl("jdbc:mysql://" + host + ":" + port + "/" + db);
+            config.setJdbcUrl("jdbc:mysql://" + host + ":" + port + "/" + db + "?useSSL=false&serverTimezone=UTC");
             config.setUsername(user);
             config.setPassword(pass);
         } else {
-            config.setJdbcUrl("jdbc:sqlite:" +
-                    plugin.getDataFolder() + "/data.db");
+            config.setJdbcUrl("jdbc:sqlite:" + plugin.getDataFolder() + "/data.db");
         }
 
         config.setMaximumPoolSize(10);
         config.setPoolName("GemsPool");
 
-        dataSource = new HikariDataSource(config);
+        this.dataSource = new HikariDataSource(config);
     }
 
     public HikariDataSource getDataSource() {
@@ -40,6 +38,6 @@ public class DatabasePool {
     }
 
     public void shutdown() {
-        dataSource.close();
+        if (dataSource != null && !dataSource.isClosed()) dataSource.close();
     }
 }
