4af1b0cff6d1bc79cdae0fbe9cbf17dd4226a53f
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-01 23:23:31 +0700

    Update PDCUtil.java

--- src/main/java/aoichan/crystal/utils/PDCUtil.java
+++ src/main/java/aoichan/crystal/utils/PDCUtil.java
@@ -1,1 +1,42 @@
+package aoichan.crystal.utils;
 
+import aoichan.crystal.AoiMain;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.*;
+
+public class PDCUtil {
+
+    private static final NamespacedKey KEY =
+            new NamespacedKey(AoiMain.get(), "gems");
+
+    public static List<String> getSocketList(ItemStack item) {
+
+        if (!item.hasItemMeta()) return new ArrayList<>();
+
+        ItemMeta meta = item.getItemMeta();
+        String raw = meta.getPersistentDataContainer()
+                .get(KEY, PersistentDataType.STRING);
+
+        if (raw == null) return new ArrayList<>();
+
+        return new ArrayList<>(Arrays.asList(raw.split(",")));
+    }
+
+    public static void setSocketList(ItemStack item, List<String> list) {
+
+        ItemMeta meta = item.getItemMeta();
+        meta.getPersistentDataContainer()
+                .set(KEY, PersistentDataType.STRING,
+                        String.join(",", list));
+
+        item.setItemMeta(meta);
+    }
+
+    public static boolean hasGemTag(ItemStack item) {
+        return !getSocketList(item).isEmpty();
+    }
+}
