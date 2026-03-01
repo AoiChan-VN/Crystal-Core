21781747433f70f724765a1ba81058568e23a861
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-01 19:37:17 +0700

    Update ReloadManager.java

--- src/main/java/aoichan/crystal/core/ReloadManager.java
+++ src/main/java/aoichan/crystal/core/ReloadManager.java
@@ -1,1 +1,16 @@
+package aoichan.crystal.core;
 
+import aoichan.crystal.AoiMain;
+
+public class ReloadManager {
+
+    public static void reload() {
+
+        AoiMain plugin = AoiMain.get();
+
+        plugin.reloadConfig();
+        plugin.getGemsManager().reload();
+
+        plugin.getLogger().info("GemsUltimate reloaded without restart.");
+    }
+}
