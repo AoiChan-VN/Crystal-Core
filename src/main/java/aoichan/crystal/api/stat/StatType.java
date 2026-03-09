5af9603285fb71dca0c6daff56559b5ff9185cad
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-10 00:43:52 +0700

    Update and rename .gitkeep to StatType.java

---
+++ src/main/java/aoichan/crystal/api/stat/StatType.java
@@ -0,0 +1,22 @@
+package aoichan.crystal.api.stat;
+
+public enum StatType {
+
+    // [!] Code: Damage
+    STR,
+
+    // [!] Code: Defense
+    DEF,
+
+    // [!] Code: Critical chance
+    CRIT,
+
+    // [!] Code: Critical damage
+    CRIT_DAMAGE,
+
+    // [!] Code: Element damage
+    ELEMENT_DAMAGE,
+
+    // [!] Code: Life steal
+    LIFESTEAL
+}
