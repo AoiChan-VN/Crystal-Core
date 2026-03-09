30d2da7ec144f3dc8db3c7fbebd15bae25187941
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-09 23:36:14 +0700

    Update and rename .gitkeep to CrystalPlugin.java

---
+++ src/main/java/aoichan/crystal/bootstrap/CrystalPlugin.java
@@ -0,0 +1,64 @@
+package aiochan.crystal.bootstrap;
+
+import aiochan.crystal.core.module.ModuleLoader;
+import aiochan.crystal.infrastructure.config.ConfigLoader;
+import aiochan.crystal.infrastructure.scheduler.TaskScheduler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CrystalPlugin extends JavaPlugin {
+
+    private static CrystalPlugin instance;
+
+    private ModuleLoader moduleLoader;
+    private ConfigLoader configLoader;
+    private TaskScheduler scheduler;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        // [!] Code: Plugin Startup
+        getLogger().info("Crystal Ultimate is awakening...");
+
+        // [!] Code: Load Config System
+        this.configLoader = new ConfigLoader(this);
+        configLoader.load();
+
+        // [!] Code: Scheduler System
+        this.scheduler = new TaskScheduler(this);
+
+        // [!] Code: Module Loader
+        this.moduleLoader = new ModuleLoader();
+        moduleLoader.loadModules();
+
+        getLogger().info("Crystal Ultimate loaded successfully.");
+    }
+
+    @Override
+    public void onDisable() {
+
+        // [!] Code: Plugin Shutdown
+        getLogger().info("Crystal Ultimate shutting down...");
+
+        if (moduleLoader != null) {
+            moduleLoader.shutdown();
+        }
+    }
+
+    public static CrystalPlugin get() {
+        return instance;
+    }
+
+    public ModuleLoader getModuleLoader() {
+        return moduleLoader;
+    }
+
+    public ConfigLoader getConfigLoader() {
+        return configLoader;
+    }
+
+    public TaskScheduler getScheduler() {
+        return scheduler;
+    }
+}
