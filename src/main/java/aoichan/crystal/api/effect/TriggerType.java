d2749661e9ef29d448bab5f5082bab83ea6310a1
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-10 14:52:29 +0700

    Update and rename .gitkeep to TriggerType.java

---
+++ src/main/java/aoichan/crystal/api/effect/TriggerType.java
@@ -0,0 +1,25 @@
+package aoichan.crystal.api.effect;
+
+public enum TriggerType {
+
+    // [!] Code: Player attack
+    ON_ATTACK,
+
+    // [!] Code: Player damaged
+    ON_DAMAGED,
+
+    // [!] Code: Critical hit
+    ON_CRIT,
+
+    // [!] Code: Kill mob/player
+    ON_KILL,
+
+    // [!] Code: Break block
+    ON_BLOCK_BREAK,
+
+    // [!] Code: Player move
+    ON_MOVE,
+
+    // [!] Code: Player jump
+    ON_JUMP
+}
