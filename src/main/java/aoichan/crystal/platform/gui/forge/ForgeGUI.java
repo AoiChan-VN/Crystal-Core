aa12b8c5eece29df4effe30cd98ba37c8f39e05b
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-10 15:51:24 +0700

    Update ForgeGUI.java

--- src/main/java/aoichan/crystal/platform/gui/forge/ForgeGUI.java
+++ src/main/java/aoichan/crystal/platform/gui/forge/ForgeGUI.java
@@ -7,19 +7,26 @@ import org.bukkit.inventory.Inventory;
 
 public class ForgeGUI extends GUI {
 
+    // [!] Code: GUI slots
+    public static final int ITEM_SLOT = 11;
+    public static final int GEM_SLOT = 15;
+    public static final int BUTTON_SLOT = 13;
+    public static final int INFO_SLOT = 22;
+
     @Override
     public void open(Player player) {
-
         player.openInventory(create());
     }
 
     @Override
     public Inventory create() {
 
-        return Bukkit.createInventory(
+        Inventory inv = Bukkit.createInventory(
                 null,
                 27,
-                "Crystal Forge"
+                "§5Crystal Forge"
         );
+
+        return inv;
     }
 }
