c4e7c35ce6a7d57b212877114f82ae7a4729ac03
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-14 19:15:50 +0700

    Update and rename GemModule.java to CrystalStat.java

---
+++ src/main/java/aoichan/crystal/gameplay/gem/CrystalStat.java
@@ -0,0 +1,16 @@
+package aoichan.crystal.gameplay.gem;
+
+public enum CrystalStat {
+
+    DAMAGE,
+    CRIT_RATE,
+    CRIT_DAMAGE,
+    DEFENSE,
+    MAX_HEALTH,
+    MAX_MANA,
+    MANA_REGEN,
+    LIFE_STEAL,
+    SKILL_DAMAGE,
+    ATTACK_SPEED;
+
+}
