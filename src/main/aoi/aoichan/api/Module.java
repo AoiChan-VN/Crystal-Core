c18a3c01f70aa102ad3239889b2d132d0b588c94
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-16 00:48:18 +0700

    Create Module.java

---
+++ src/main/aoi/aoichan/api/Module.java
@@ -0,0 +1,14 @@
+package aoi.aoichan.api;
+
+public interface Module {
+
+    // 【!】Code: tên module
+    String getName();
+
+    // 【!】Code: bật module
+    void enable();
+
+    // 【!】Code: tắt module
+    void disable();
+
+} 
