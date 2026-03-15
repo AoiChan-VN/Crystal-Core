c947d39992ebfbc09560d36630db33b177d9a176
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-15 10:02:56 +0700

    Create EngineModule.java

---
+++ src/main/java/aoi/crystalengine/module/EngineModule.java
@@ -0,0 +1,16 @@
+package com.aoi.crystalengine.module;
+
+/*
+#【!】Code:
+Interface cho mọi module trong hệ sinh thái Crystal.
+*/
+
+public interface EngineModule {
+
+    void enable();
+
+    void disable();
+
+    String getName();
+
+} 
