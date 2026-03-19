1f027b8f58f5039c10b163d1f56e29f322b8db2c
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-19 18:02:15 +0700

    Create EventBus.java

---
+++ src/main/java/aoichan/eventbus/EventBus.java
@@ -0,0 +1,31 @@
+package aoichan.eventbus;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class EventBus {
+
+    private static final Map<Class<?>, List<ListenerMethod>> listeners = new HashMap<>();
+
+    public static void register(Object obj) {
+        for (Method m : obj.getClass().getDeclaredMethods()) {
+            if (!m.isAnnotationPresent(Subscribe.class)) continue;
+
+            Class<?> param = m.getParameterTypes()[0];
+            listeners.computeIfAbsent(param, k -> new ArrayList<>())
+                    .add(new ListenerMethod(obj, m));
+        }
+    }
+
+    public static void post(Object event) {
+        List<ListenerMethod> list = listeners.get(event.getClass());
+        if (list == null) return;
+
+        for (ListenerMethod lm : list) {
+            try {
+                lm.method.invoke(lm.owner, event);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    } 
