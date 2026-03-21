8846fe5262fb0f87998109db9bcf6b33c3866b56
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-22 02:32:00 +0700

    Update Plugin.java

--- src/main/java/vn/aoi/onii/Plugin.java
+++ src/main/java/vn/aoi/onii/Plugin.java
@@ -1,1 +1,26 @@
+// File: src/main/java/vn/aoi/onii/Plugin.java
+package vn.aoi.onii;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+
+    private static Plugin instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        //【❅】 Initialize systems
+        getLogger().info("Plugin Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        //【❅】 Shutdown systems safely
+        getLogger().info("Plugin Disabled!");
+    }
+
+    public static Plugin getInstance() {
+        return instance;
+    }
+}
