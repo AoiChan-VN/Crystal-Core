aba18255df0e94ae687487bfec158f8e70e5beab
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-22 20:05:50 +0700

    Update Main.java

--- src/main/java/vn/aoi/onii/Main.java
+++ src/main/java/vn/aoi/onii/Main.java
@@ -1,35 +1,21 @@
+// File: src/main/java/vn/aoi/onii/Main.java
 package vn.aoi.onii;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import vn.aoi.onii.bootstrap.Bootstrap;
+import vn.aoi.onii.core.Bootstrap;
 
-//【❅】 Main plugin entry
-public class Main extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
-    private static Main instance;
     private Bootstrap bootstrap;
-
-    public static Main getInstance() {
-        return instance;
-    }
 
     @Override
     public void onEnable() {
-        instance = this;
-
-        //【❅】 Bootstrap toàn bộ hệ thống
         this.bootstrap = new Bootstrap(this);
-        bootstrap.enable();
-
-        getLogger().info("AoiRPG Enabled!");
+        this.bootstrap.init();
     }
 
     @Override
     public void onDisable() {
-        if (bootstrap != null) {
-            bootstrap.disable();
-        }
-
-        getLogger().info("AoiRPG Disabled!");
+        if (bootstrap != null) bootstrap.shutdown();
     }
 }
