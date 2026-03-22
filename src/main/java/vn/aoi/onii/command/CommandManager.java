cc2958bd19c26f59bdf2694587f0bf751e0e0aab
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-22 20:17:42 +0700

    Create CommandManager.java

---
+++ src/main/java/vn/aoi/onii/command/CommandManager.java
@@ -0,0 +1,21 @@
+// File: command/CommandManager.java
+package vn.aoi.onii.command;
+
+import org.bukkit.command.*;
+import vn.aoi.onii.Main;
+import vn.aoi.onii.player.PlayerManager;
+
+public class CommandManager {
+
+    private final Main plugin;
+    private final PlayerManager playerManager;
+
+    public CommandManager(Main plugin, PlayerManager pm) {
+        this.plugin = plugin;
+        this.playerManager = pm;
+    }
+
+    public void register() {
+        plugin.getCommand("onii").setExecutor(new RootCommand(playerManager));
+    }
+} 
