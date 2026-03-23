16b13765a77c0ec11ec856290266fe83016f920c
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-24 02:26:40 +0700

    Create Skill.java

---
+++ src/main/java/vn/aoi/onii/skill/Skill.java
@@ -0,0 +1,13 @@
+package vn.aoi.onii.skill;
+
+import org.bukkit.entity.Player;
+import vn.aoi.onii.classsystem.ClassContext;
+
+public interface Skill {
+
+    String getId();
+
+    long getCooldown(); // ms
+
+    void execute(ClassContext ctx);
+} 
