dd0b2f7743d69c71edc7e8b3fb28500a21151ebf
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-23 22:51:14 +0700

    Create ClassContext.java

---
+++ src/main/java/vn/aoi/onii/classsystem/ClassContext.java
@@ -0,0 +1,18 @@
+package vn.aoi.onii.classsystem;
+
+import org.bukkit.entity.Player;
+import vn.aoi.onii.player.PlayerData;
+
+public class ClassContext {
+
+    private final Player player;
+    private final PlayerData data;
+
+    public ClassContext(Player player, PlayerData data) {
+        this.player = player;
+        this.data = data;
+    }
+
+    public Player getPlayer() { return player; }
+    public PlayerData getData() { return data; }
+} 
