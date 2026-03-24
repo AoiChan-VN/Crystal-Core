245f9c7ecb1858b1154de4a1993874173cf944cc
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-25 01:25:46 +0700

    Create Buff.java

---
+++ src/main/java/vn/aoi/onii/combat/Buff.java
@@ -0,0 +1,24 @@
+package vn.aoi.onii.combat;
+
+import java.util.*;
+
+public class Buff {
+
+    public String id;
+    public long expireAt;
+
+    public double str;
+    public double agi;
+    public double intel;
+    public double def;
+    public double crit;
+
+    public Buff(String id, long duration) {
+        this.id = id;
+        this.expireAt = System.currentTimeMillis() + duration;
+    }
+
+    public boolean isExpired() {
+        return System.currentTimeMillis() > expireAt;
+    }
+} 
