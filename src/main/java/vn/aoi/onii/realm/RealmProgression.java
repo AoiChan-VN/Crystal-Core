be49f52c626f2c74224a00b84ffc063e20546a08
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-27 00:10:29 +0700

    Update RealmProgression.java

--- src/main/java/vn/aoi/onii/realm/RealmProgression.java
+++ src/main/java/vn/aoi/onii/realm/RealmProgression.java
@@ -8,6 +8,7 @@ public class RealmProgression {
     public static int getRequiredExp(Realm realm, Stage stage) {
 
         return switch (stage) {
+            case NHAP_MON -> 50;
             case SO_KY -> 100;
             case TRUNG_KY -> 300;
             case HAU_KY -> 600;
@@ -17,6 +18,7 @@ public class RealmProgression {
 
     public static Stage nextStage(Stage stage) {
         return switch (stage) {
+            case NHAP_MON -> Stage.SO_KY;
             case SO_KY -> Stage.TRUNG_KY;
             case TRUNG_KY -> Stage.HAU_KY;
             case HAU_KY -> Stage.DINH_PHONG;
