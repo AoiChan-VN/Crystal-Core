4d0eb470d97424b5f239eeef606f988ae696e2a3
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 07:07:38 +0700

    Update Main.java

--- src/main/java/vn/aoi/onii/Main.java
+++ src/main/java/vn/aoi/onii/Main.java
@@ -1,24 +1,41 @@
 package vn.aoi.onii;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import vn.aoi.onii.commands.AoiCommand;
+import vn.aoi.onii.database.SQLite;
+import vn.aoi.onii.listeners.ChatListener;
+import vn.aoi.onii.player.PlayerManager;
 
 public class Main extends JavaPlugin {
 
     private static Main instance;
+    private SQLite sqlite;
+    private PlayerManager playerManager;
 
     @Override
     public void onEnable() {
         instance = this;
-        Bukkit.getLogger().info("AoiChan đã thức tỉnh linh lực!");
-    }
 
-    @Override
-    public void onDisable() {
-        Bukkit.getLogger().info("AoiChan đã phi thăng...");
+        saveDefaultConfig();
+
+        sqlite = new SQLite(this);
+        sqlite.connect();
+        sqlite.createTable();
+
+        playerManager = new PlayerManager(sqlite);
+
+        getCommand("aoi").setExecutor(new AoiCommand(playerManager));
+
+        getServer().getPluginManager().registerEvents(new ChatListener(playerManager), this);
+
+        saveResource("shop.yml", false);
     }
 
     public static Main getInstance() {
         return instance;
+    }
+
+    public SQLite getSQLite() {
+        return sqlite;
     }
 }
