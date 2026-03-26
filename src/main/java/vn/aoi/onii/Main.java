a35620812dc10e75ca25836c027f9485add52aed
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-26 10:02:13 +0700

    Update Main.java

--- src/main/java/vn/aoi/onii/Main.java
+++ src/main/java/vn/aoi/onii/Main.java
@@ -1,22 +1,31 @@
 package vn.aoi.onii;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import vn.aoi.onii.commands.AoiCommand;
 import vn.aoi.onii.database.Database;
 import vn.aoi.onii.listeners.ChatListener;
 import vn.aoi.onii.player.PlayerManager;
+import vn.aoi.onii.shop.ShopListener;
+
+import java.io.File;
 
 public class Main extends JavaPlugin {
 
     private static Main instance;
     private Database database;
     private PlayerManager playerManager;
+    private Economy econ;
 
     @Override
     public void onEnable() {
         instance = this;
 
         saveDefaultConfig();
+        saveResource("shop.yml", false);
+
+        setupEconomy();
 
         database = new Database(this);
         database.connect();
@@ -25,7 +34,9 @@ public class Main extends JavaPlugin {
         playerManager = new PlayerManager(database);
 
         getCommand("aoi").setExecutor(new AoiCommand(playerManager));
+
         getServer().getPluginManager().registerEvents(new ChatListener(playerManager), this);
+        getServer().getPluginManager().registerEvents(new ShopListener(playerManager, econ), this);
     }
 
     @Override
@@ -35,5 +46,25 @@ public class Main extends JavaPlugin {
 
     public static Main getInstance() {
         return instance;
+    }
+
+    public File getShopFile() {
+        return new File(getDataFolder(), "shop.yml");
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) return false;
+
+        RegisteredServiceProvider<Economy> rsp =
+                getServer().getServicesManager().getRegistration(Economy.class);
+
+        if (rsp == null) return false;
+
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public Economy getEcon() {
+        return econ;
     }
 }
