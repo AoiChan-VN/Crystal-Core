e57d4845c7207ec45bfb960502a7b4641031d225
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-28 16:57:39 +0700

    Update WeaponTier.java

--- src/main/java/vn/aoi/onii/enums/WeaponTier.java
+++ src/main/java/vn/aoi/onii/enums/WeaponTier.java
@@ -1,5 +1,36 @@
 package vn.aoi.onii.enums;
 
 public enum WeaponTier {
-    PHAP_KHI, LINH_KHI, PHAP_BAO, CO_BAO, LINH_BAO, TIEN_KHI, THAN_KHI
+
+    PHAP_KHI("Pháp Khí", 1),
+    LINH_KHI("Linh Khí", 2),
+    PHAP_BAO("Pháp Bảo", 3),
+    CO_BAO("Cổ Bảo", 4),
+    LINH_BAO("Linh Bảo", 5),
+    TIEN_KHI("Tiên Khí", 6),
+    THAN_KHI("Thần Khí", 7);
+
+    private final String display;
+    private final int level;
+
+    WeaponTier(String display, int level) {
+        this.display = display;
+        this.level = level;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public static WeaponTier fromString(String s) {
+        try {
+            return WeaponTier.valueOf(s.toUpperCase());
+        } catch (Exception e) {
+            return PHAP_KHI;
+        }
+    }
 }
