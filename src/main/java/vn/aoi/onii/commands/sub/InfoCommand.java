7a7fe0cfd0d4d1f577b9a8220e595c88953075bf
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-29 13:12:05 +0700

    Update InfoCommand.java

--- src/main/java/vn/aoi/onii/commands/sub/InfoCommand.java
+++ src/main/java/vn/aoi/onii/commands/sub/InfoCommand.java
@@ -1,7 +1,11 @@
 package vn.aoi.onii.commands.sub;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import vn.aoi.onii.Main;
 import vn.aoi.onii.commands.core.AbstractCommand;
+import vn.aoi.onii.player.PlayerData;
 
 import java.util.Collections;
 import java.util.List;
@@ -15,7 +19,7 @@ public class InfoCommand extends AbstractCommand {
 
     @Override
     public String getDescription() {
-        return "Xem thông tin";
+        return "Xem thông tin tu tiên";
     }
 
     @Override
@@ -25,7 +29,28 @@ public class InfoCommand extends AbstractCommand {
 
     @Override
     public boolean execute(CommandSender sender, String[] args) {
-        sender.sendMessage("§aThông tin player...");
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Chỉ player dùng được!");
+            return true;
+        }
+
+        PlayerData data = Main.getInstance()
+                .getPlayerManager()
+                .getPlayer(player.getUniqueId());
+
+        sender.sendMessage("§6═══════ 《 THÔNG TIN 》 ═══════");
+        sender.sendMessage("§eDanh tính: §f" + player.getName());
+        sender.sendMessage("§eTông môn: §f" + data.getSect());
+
+        sender.sendMessage("§eCảnh giới: §f" + data.getRealm().getDisplay());
+        sender.sendMessage("§eTu vi: §f" + data.getStage().getDisplay());
+
+        sender.sendMessage("§eCông pháp: §f" + data.getTechnique().getDisplay());
+        sender.sendMessage("§ePháp bảo: §f" + data.getWeapon().getDisplay());
+
+        sender.sendMessage("§6═══════════════════════════");
+
         return true;
     }
 
@@ -33,4 +58,4 @@ public class InfoCommand extends AbstractCommand {
     public List<String> tab(CommandSender sender, String[] args) {
         return Collections.emptyList();
     }
-} 
+}
