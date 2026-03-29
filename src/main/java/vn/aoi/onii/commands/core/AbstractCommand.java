c0c786dfeaf910176aae79cdacbad70db0d7ca17
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-29 09:09:18 +0700

    Create AbstractCommand.java

---
+++ src/main/java/vn/aoi/onii/commands/core/AbstractCommand.java
@@ -0,0 +1,15 @@
+package vn.aoi.onii.commands.core;
+
+import org.bukkit.command.CommandSender;
+
+public abstract class AbstractCommand implements SubCommand {
+
+    @Override
+    public String getPermission() {
+        return null;
+    }
+
+    public boolean hasPermission(CommandSender sender) {
+        return getPermission() == null || sender.hasPermission(getPermission());
+    }
+}
