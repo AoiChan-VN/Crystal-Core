02459b74c1b1402562a59fc3a78cafab4dfc882d
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-29 20:20:40 +0700

    Update QuestListener.java

--- src/main/java/vn/aoi/onii/quest/QuestListener.java
+++ src/main/java/vn/aoi/onii/quest/QuestListener.java
@@ -1,95 +1,23 @@
 package vn.aoi.onii.quest;
 
-import net.milkbowl.vault.economy.Economy;
-import org.bukkit.event.*;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
 import vn.aoi.onii.player.PlayerManager;
 
 public class QuestListener implements Listener {
 
     private final QuestManager questManager;
     private final PlayerManager playerManager;
-    private final Economy econ;
 
-    public QuestListener(QuestManager qm, PlayerManager pm, Economy econ) {
-        this.questManager = qm;
-        this.playerManager = pm;
-        this.econ = econ;
+    public QuestListener(QuestManager questManager, PlayerManager playerManager) {
+        this.questManager = questManager;
+        this.playerManager = playerManager;
     }
 
     @EventHandler
-    public void kill(EntityDeathEvent e) {
-        if (!(e.getEntity().getKiller() instanceof Player player)) return;
-
-        var config = questManager.getConfig();
-
-        for (String key : config.getConfigurationSection("quests").getKeys(false)) {
-
-            String path = "quests." + key;
-
-            if (!config.getString(path + ".type").equalsIgnoreCase("KILL")) continue;
-
-            if (!e.getEntity().getType().name().equalsIgnoreCase(config.getString(path + ".target"))) continue;
-
-            questManager.addProgress(player.getUniqueId(), key, 1);
-
-            int progress = questManager.getProgress(player.getUniqueId(), key);
-            int need = config.getInt(path + ".amount");
-
-            if (progress >= need) {
-
-                double money = config.getDouble(path + ".reward.money");
-                int exp = config.getInt(path + ".reward.exp");
-
-                econ.depositPlayer(player, money);
-
-                var data = playerManager.get(player.getUniqueId(), player.getName());
-                data.setExp(data.getExp() + exp);
-                playerManager.save(data);
-
-                player.sendMessage("§aHoàn thành nhiệm vụ! +" + exp + " EXP");
-
-                questManager.reset(player.getUniqueId(), key);
-            }
-        }
+    public void onJoin(PlayerJoinEvent e) {
+        playerManager.load(e.getPlayer());
     }
-
-    @EventHandler
-    public void breakBlock(BlockBreakEvent e) {
-
-        Player player = e.getPlayer();
-        var config = questManager.getConfig();
-
-        for (String key : config.getConfigurationSection("quests").getKeys(false)) {
-
-            String path = "quests." + key;
-
-            if (!config.getString(path + ".type").equalsIgnoreCase("BREAK")) continue;
-
-            if (!e.getBlock().getType().name().equalsIgnoreCase(config.getString(path + ".target"))) continue;
-
-            questManager.addProgress(player.getUniqueId(), key, 1);
-
-            int progress = questManager.getProgress(player.getUniqueId(), key);
-            int need = config.getInt(path + ".amount");
-
-            if (progress >= need) {
-
-                double money = config.getDouble(path + ".reward.money");
-                int exp = config.getInt(path + ".reward.exp");
-
-                econ.depositPlayer(player, money);
-
-                var data = playerManager.get(player.getUniqueId(), player.getName());
-                data.setExp(data.getExp() + exp);
-                playerManager.save(data);
-
-                player.sendMessage("§aHoàn thành nhiệm vụ! +" + exp + " EXP");
-
-                questManager.reset(player.getUniqueId(), key);
-            }
-        }
-    }
-} 
+}
