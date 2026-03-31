62f4857e1af6951bc0b3b2a16a90ef0e87462ca7
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 06:09:40 +0700

    Update PlayerData.java

--- src/main/java/vn/aoi/onii/player/PlayerData.java
+++ src/main/java/vn/aoi/onii/player/PlayerData.java
@@ -1,30 +1,15 @@
 package vn.aoi.onii.player;
 
 public class PlayerData {
-
     private String rank;
     private int level;
     private int exp;
 
-    public PlayerData(String rank, int level, int exp) {
-        this.rank = rank;
-        this.level = level;
-        this.exp = exp;
-    }
-
-    public String getRank() { return rank; }
-    public int getLevel() { return level; }
-    public int getExp() { return exp; }
-
-    public void addExp(int amount) {
-        this.exp += amount;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-} 
+    public PlayerData(String r,int l,int e){rank=r;level=l;exp=e;}
+    public String getRank(){return rank;}
+    public int getLevel(){return level;}
+    public int getExp(){return exp;}
+    public void addExp(int a){exp+=a;}
+    public void setLevel(int l){level=l;}
+    public void setRank(String r){rank=r;}
+}
