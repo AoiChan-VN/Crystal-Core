696764194bd3f07d8c49deaea40394f1d1773eb8
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 11:35:42 +0700

    Update AbstractCommand.java

--- src/main/java/vn/aoi/onii/commands/framework/AbstractCommand.java
+++ src/main/java/vn/aoi/onii/commands/framework/AbstractCommand.java
@@ -1,7 +1,6 @@
 package vn.aoi.onii.commands.framework;
 
 import vn.aoi.onii.Main;
-import vn.aoi.onii.config.MessageManager;
 
 public abstract class AbstractCommand implements ICommandModule {
 
@@ -10,21 +9,4 @@ public abstract class AbstractCommand implements ICommandModule {
     public AbstractCommand(Main plugin) {
         this.plugin = plugin;
     }
-
-    protected boolean checkCooldown(org.bukkit.command.CommandSender sender,
-                                    String command,
-                                    int seconds) {
-
-        String key = sender.getName() + ":" + command;
-
-        if (CooldownManager.isOnCooldown(key, seconds)) {
-            long remain = CooldownManager.getRemaining(key, seconds);
-            sender.sendMessage(MessageManager.get("cooldown")
-                    .replace("{time}", String.valueOf(remain)));
-            return false;
-        }
-
-        CooldownManager.setCooldown(key);
-        return true;
-    }
-} 
+}
