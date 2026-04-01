23c38f9077da4054462f7ec70b046911238d7946
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 13:10:25 +0700

    Delete src/main/java/vn/aoi/onii directory

--- src/main/java/vn/aoi/onii/commands/framework/ICommandModule.java
+++
@@ -1,7 +0,0 @@
-package vn.aoi.onii.commands.framework;
-
-import cloud.commandframework.annotations.AnnotationParser;
-
-public interface ICommandModule {
-    void register(AnnotationParser<?> parser);
-}
--- src/main/java/vn/aoi/onii/commands/framework/annotations/CommandCooldown.java
+++
@@ -1,9 +0,0 @@
-package vn.aoi.onii.commands.framework.annotations;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface CommandCooldown {
-    int seconds();
-}
