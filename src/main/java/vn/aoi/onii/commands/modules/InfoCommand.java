18a6a71a7f3dc71188e9f43e4f7eb7761d5fa421
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 11:49:35 +0700

    Update InfoCommand.java

--- src/main/java/vn/aoi/onii/commands/modules/InfoCommand.java
+++ src/main/java/vn/aoi/onii/commands/modules/InfoCommand.java
@@ -1,16 +1,32 @@
-@CommandMethod("aoi info")
-@CommandCooldown(seconds = 3)
-public void info(Player player) {
+package vn.aoi.onii.commands.modules;
 
-    if (!checkCooldown(player, "info", 3)) return;
+import cloud.commandframework.annotations.CommandMethod;
+import org.bukkit.entity.Player;
+import vn.aoi.onii.commands.framework.AbstractCommand;
+import vn.aoi.onii.config.MessageManager;
 
-    var data = plugin.getPlayerManager().get(player);
+public class InfoCommand extends AbstractCommand {
 
-    player.sendMessage(MessageManager.get("info.header"));
-    player.sendMessage(MessageManager.get("info.realm")
-            .replace("{realm}", data.getRealm()));
-    player.sendMessage(MessageManager.get("info.level")
-            .replace("{level}", String.valueOf(data.getLevel())));
-    player.sendMessage(MessageManager.get("info.exp")
-            .replace("{exp}", String.valueOf(data.getExp())));
-} 
+    public InfoCommand(vn.aoi.onii.Main plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void register(cloud.commandframework.annotations.AnnotationParser<?> parser) {
+        parser.parse(this);
+    }
+
+    @CommandMethod("aoi info")
+    public void info(Player player) {
+
+        var data = plugin.getPlayerManager().get(player);
+
+        player.sendMessage(MessageManager.get("info.header"));
+        player.sendMessage(MessageManager.get("info.realm")
+                .replace("{realm}", data.getRealm()));
+        player.sendMessage(MessageManager.get("info.level")
+                .replace("{level}", String.valueOf(data.getLevel())));
+        player.sendMessage(MessageManager.get("info.exp")
+                .replace("{exp}", String.valueOf(data.getExp())));
+    }
+}
