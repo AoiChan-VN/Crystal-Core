f1a091ca58d154fed3a39b7046b0a31d62dd1f57
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-02 00:04:09 +0700

    Create ColorUtil.java

---
+++ src/main/java/aoi/onii/util/ColorUtil.java
@@ -0,0 +1,10 @@
+package vn.aoi.onii.util;
+
+import org.bukkit.ChatColor;
+
+public class ColorUtil {
+
+    public static String color(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+} 
