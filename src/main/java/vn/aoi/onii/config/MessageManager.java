d93804fe5a351bda6bdc3732da305e237f494e9d
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-01 11:11:32 +0700

    Update MessageManager.java

--- src/main/java/vn/aoi/onii/config/MessageManager.java
+++ src/main/java/vn/aoi/onii/config/MessageManager.java
@@ -1,18 +1,27 @@
 package vn.aoi.onii.config;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import vn.aoi.onii.Main;
+
+import java.io.File;
 
 public class MessageManager {
 
     private static FileConfiguration config;
 
     public static void init(Main plugin) {
-        plugin.saveResource("messages.yml", false);
-        config = plugin.getConfig(); // hoặc load riêng file nếu muốn
+        File file = new File(plugin.getDataFolder(), "messages.yml");
+
+        if (!file.exists()) {
+            plugin.saveResource("messages.yml", false);
+        }
+
+        config = YamlConfiguration.loadConfiguration(file);
     }
 
     public static String get(String path) {
-        return config.getString("messages." + path, "Missing message: " + path);
+        return config.getString("messages." + path, "Missing: " + path)
+                .replace("&", "§");
     }
-} 
+}
