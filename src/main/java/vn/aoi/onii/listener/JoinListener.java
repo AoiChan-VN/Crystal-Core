4cb0a793bbf94e530572f3e68e45943f759f5b4b
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 01:03:26 +0700

    Update JoinListener.java

--- src/main/java/vn/aoi/onii/listener/JoinListener.java
+++ src/main/java/vn/aoi/onii/listener/JoinListener.java
@@ -11,30 +11,18 @@ import java.sql.*;
 public class JoinListener implements Listener {
 
     @EventHandler
-    public void join(PlayerJoinEvent e) {
+    public void onJoin(PlayerJoinEvent e){
+        try{
+            var id=e.getPlayer().getUniqueId();
+            var ps=Main.get().db().get().prepareStatement("SELECT * FROM players WHERE uuid=?");
+            ps.setString(1,id.toString());
+            var rs=ps.executeQuery();
 
-        var uuid = e.getPlayer().getUniqueId();
-
-        try {
-            var ps = Main.getInstance().getDatabase().getConnection()
-                    .prepareStatement("SELECT * FROM players WHERE uuid=?");
-
-            ps.setString(1, uuid.toString());
-            var rs = ps.executeQuery();
-
-            if (rs.next()) {
-                PlayerManager.set(new PlayerData(
-                        uuid,
-                        rs.getString("realm"),
-                        rs.getInt("level"),
-                        rs.getInt("exp")
-                ));
-            } else {
-                PlayerManager.set(new PlayerData(uuid, "Phàm nhân", 1, 0));
+            if(rs.next()){
+                PlayerManager.set(new PlayerData(id,rs.getString("realm"),rs.getInt("level"),rs.getInt("exp")));
+            }else{
+                PlayerManager.set(new PlayerData(id,"Phàm nhân",1,0));
             }
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        }catch(Exception ex){ex.printStackTrace();}
     }
-} 
+}
