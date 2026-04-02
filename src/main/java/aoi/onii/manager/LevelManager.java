5ce14c7cea31949fc147468ab65566ce407096f5
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-02 23:50:22 +0700

    Update LevelManager.java

--- src/main/java/aoi/onii/manager/LevelManager.java
+++ src/main/java/aoi/onii/manager/LevelManager.java
@@ -8,15 +8,15 @@ import vn.aoi.onii.task.ThunderTask;
 
 public class LevelManager {
 
-    public static void checkLevelUp(Player player, PlayerData data) {
+    public static void check(Player player, PlayerData data) {
 
         while (true) {
+
             Realm realm = RealmManager.get(data.getRealm());
             if (realm == null) return;
 
             int level = data.getLevel();
 
-            // Max level → chuyển cảnh giới
             if (level >= realm.getMaxLevel()) {
 
                 if (realm.getNext() == null) return;
@@ -28,7 +28,6 @@ public class LevelManager {
 
                 data.setRealm(realm.getNext());
                 data.setLevel(1);
-                player.sendMessage("§dĐột phá cảnh giới: §e" + realm.getNext());
                 continue;
             }
 
@@ -40,7 +39,7 @@ public class LevelManager {
             data.addExp(-info.getExp());
             data.setLevel(level + 1);
 
-            player.sendMessage("§aĐột phá tầng " + (level + 1));
+            player.sendMessage("§aĐột phá!");
         }
     }
 }
