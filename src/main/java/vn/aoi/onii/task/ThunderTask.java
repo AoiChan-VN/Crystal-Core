30b30dc2df78ffc3e99da2d758868e5ab5c5ac07
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 01:05:03 +0700

    Update ThunderTask.java

--- src/main/java/vn/aoi/onii/task/ThunderTask.java
+++ src/main/java/vn/aoi/onii/task/ThunderTask.java
@@ -3,27 +3,23 @@ package vn.aoi.onii.task;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.entity.Player;
 import vn.aoi.onii.Main;
-
 import java.util.Random;
 
 public class ThunderTask extends BukkitRunnable {
 
+    private int t=30;
     private final Player p;
-    private int t = 30;
-    private final Random r = new Random();
+    private final Random r=new Random();
 
-    public ThunderTask(Player p) { this.p = p; }
+    public ThunderTask(Player p){this.p=p;}
 
-    public void run() {
-        if (--t <= 0) { cancel(); return; }
-
-        var loc = p.getLocation().clone().add(
-                r.nextInt(10)-5,0,r.nextInt(10)-5);
-
-        p.getWorld().strikeLightning(loc);
+    public void run(){
+        if(--t<=0){cancel();return;}
+        var l=p.getLocation().clone().add(r.nextInt(10)-5,0,r.nextInt(10)-5);
+        p.getWorld().strikeLightning(l);
     }
 
-    public void start() {
-        runTaskTimer(Main.getInstance(),0,20);
+    public void start(){
+        runTaskTimer(Main.get(),0,20);
     }
-} 
+}
