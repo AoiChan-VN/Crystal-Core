f0fbd85ad9df1b27da7abe755be00ee422ff477d
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 01:00:15 +0700

    Update LevelManager.java

--- src/main/java/vn/aoi/onii/manager/LevelManager.java
+++ src/main/java/vn/aoi/onii/manager/LevelManager.java
@@ -1,54 +1,45 @@
 package vn.aoi.onii.manager;
 
 import org.bukkit.entity.Player;
+import vn.aoi.onii.config.ConfigManager;
 import vn.aoi.onii.data.PlayerData;
-import vn.aoi.onii.model.Realm;
 import vn.aoi.onii.task.ThunderTask;
 
 public class LevelManager {
 
-    public static void check(Player p, PlayerData d) {
+    public static void check(Player p, PlayerData d){
 
-        while (true) {
+        while(true){
 
-            Realm r = RealmManager.get(d.getRealm());
-            if (r == null) return;
+            String path="realms."+d.realm;
 
-            // Phàm nhân kiểu exp-required
-            if (r.getLevels().isEmpty()) {
-                int req = vn.aoi.onii.config.ConfigManager.realms
-                        .getInt("realms." + r.getName() + ".exp-required");
+            if(ConfigManager.realms.contains(path+".exp-required")){
+                int req=ConfigManager.realms.getInt(path+".exp-required");
 
-                if (d.getExp() < req) return;
+                if(d.exp<req) return;
 
-                d.addExp(-req);
-                d.setRealm(r.getNext());
-                d.setLevel(1);
+                d.exp-=req;
+                d.realm=ConfigManager.realms.getString(path+".next-rank");
+                d.level=1;
                 continue;
             }
 
-            int lvl = d.getLevel();
+            int need=ConfigManager.realms.getInt(path+".levels."+d.level+".exp");
+            if(d.exp<need) return;
 
-            if (lvl >= r.getMax()) {
+            d.exp-=need;
+            d.level++;
 
-                if (r.getNext() == null) return;
+            int max=ConfigManager.realms.getInt(path+".max-level");
 
-                if (r.isTrib()) {
+            if(d.level>max){
+                if(ConfigManager.realms.getBoolean(path+".is-thien-kiep")){
                     new ThunderTask(p).start();
                     return;
                 }
-
-                d.setRealm(r.getNext());
-                d.setLevel(1);
-                continue;
+                d.realm=ConfigManager.realms.getString(path+".next-rank");
+                d.level=1;
             }
-
-            int need = r.getLevels().get(lvl).getExp();
-
-            if (d.getExp() < need) return;
-
-            d.addExp(-need);
-            d.setLevel(lvl + 1);
         }
     }
 }
