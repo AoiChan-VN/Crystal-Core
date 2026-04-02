b2454fcb59d434ea77899d0a7e08f4a4d8660597
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-02 23:49:21 +0700

    Update PlayerManager.java

--- src/main/java/aoi/onii/manager/PlayerManager.java
+++ src/main/java/aoi/onii/manager/PlayerManager.java
@@ -1,27 +1,24 @@
 package vn.aoi.onii.manager;
 
-import org.bukkit.entity.Player;
 import vn.aoi.onii.data.PlayerData;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class PlayerManager {
 
-    private static final HashMap<UUID, PlayerData> cache = new HashMap<>();
+    private static final Map<UUID, PlayerData> cache = new HashMap<>();
 
-    public static PlayerData get(Player player) {
-        return cache.get(player.getUniqueId());
-    }
-
-    public static void addExp(Player player, int amount) {
-        PlayerData data = get(player);
-        data.addExp(amount);
-
-        LevelManager.checkLevelUp(player, data);
+    public static PlayerData get(UUID uuid) {
+        return cache.get(uuid);
     }
 
     public static void set(PlayerData data) {
         cache.put(data.getUuid(), data);
+    }
+
+    public static Map<UUID, PlayerData> getAll() {
+        return cache;
     }
 }
