28dbc6312be2938cd77e5a1e43deaf4fcdaaa8dc
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 00:56:38 +0700

    Update PlayerManager.java

--- src/main/java/vn/aoi/onii/manager/PlayerManager.java
+++ src/main/java/vn/aoi/onii/manager/PlayerManager.java
@@ -1,26 +1,14 @@
 package vn.aoi.onii.manager;
 
 import vn.aoi.onii.data.PlayerData;
-
 import java.util.*;
 
 public class PlayerManager {
 
-    private static final Map<UUID, PlayerData> cache = new HashMap<>();
+    private static final Map<UUID,PlayerData> map=new HashMap<>();
 
-    public static PlayerData get(UUID uuid) {
-        return cache.get(uuid);
-    }
-
-    public static void set(PlayerData data) {
-        cache.put(data.getUuid(), data);
-    }
-
-    public static void remove(UUID uuid) {
-        cache.remove(uuid);
-    }
-
-    public static Collection<PlayerData> all() {
-        return cache.values();
-    }
-} 
+    public static PlayerData get(UUID u){return map.get(u);}
+    public static void set(PlayerData d){map.put(d.uuid,d);}
+    public static void remove(UUID u){map.remove(u);}
+    public static Collection<PlayerData> all(){return map.values();}
+}
