5b91dd65d3fe55c5160eef9054699841979a9df2
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-03 09:12:57 +0700

    Update MobKillListener.java

--- src/main/java/aoi/java/vn/aoi/onii/listener/MobKillListener.java
+++ src/main/java/aoi/java/vn/aoi/onii/listener/MobKillListener.java
@@ -5,15 +5,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import vn.aoi.onii.manager.CultivationService;
-import vn.aoi.onii.manager.PlayerManager;
 
 public class MobKillListener implements Listener {
 
-    private final PlayerManager playerManager;
     private final CultivationService cultivationService;
 
-    public MobKillListener(PlayerManager playerManager, CultivationService cultivationService) {
-        this.playerManager = playerManager;
+    public MobKillListener(CultivationService cultivationService) {
         this.cultivationService = cultivationService;
     }
 
@@ -22,11 +19,8 @@ public class MobKillListener implements Listener {
         Player killer = event.getEntity().getKiller();
         if (killer == null) return;
 
-        var cultivator = playerManager.get(killer.getUniqueId());
-        if (cultivator == null) return;
+        double exp = 5; // TODO: load config
 
-        double exp = 5; // TODO: load from config mobs-exp
-
-        cultivationService.addExp(cultivator, exp);
+        cultivationService.addExp(killer, exp);
     }
-} 
+}
