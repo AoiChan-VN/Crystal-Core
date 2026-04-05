e6ce3a3cb6fe2d09b69306c3551347c3876dea76
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-05 19:24:57 +0700

    Update Cultivator.java

--- src/main/java/aoi/java/vn/aoi/onii/model/Cultivator.java
+++ src/main/java/aoi/java/vn/aoi/onii/model/Cultivator.java
@@ -14,8 +14,8 @@ public class Cultivator {
     private UUID uuid;
 
     private String realm;     // "Luyện khí", "Trúc cơ", ...
-    private int level;        // Level hiện tại
-    private double exp;       // EXP hiện tại
+    private int level;        // Tu vi hiện tại ( sơ,trung,.. )
+    private double exp;       // Linh khí hiện tại
 
     private long lastUpdated; // dùng cho cache / autosave
 }
