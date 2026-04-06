233f9d654901f0ac84c838ed6bed31da8d8fe024
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-06 17:36:45 +0700

    Update ACFCompletion.java

--- src/main/java/vn/aoi/onii/commands/context/ACFCompletion.java
+++ src/main/java/vn/aoi/onii/commands/context/ACFCompletion.java
@@ -2,6 +2,7 @@ package vn.aoi.onii.commands.context;
 
 import co.aikar.commands.PaperCommandManager;
 import org.bukkit.Bukkit;
+
 import vn.aoi.onii.manager.RealmManager;
 
 import java.util.stream.Collectors;
@@ -10,7 +11,6 @@ public class ACFCompletion {
 
     public static void register(PaperCommandManager manager, RealmManager realmManager) {
 
-        // 👤 Online players
         manager.getCommandCompletions().registerCompletion("players",
                 c -> Bukkit.getOnlinePlayers()
                         .stream()
@@ -18,7 +18,6 @@ public class ACFCompletion {
                         .collect(Collectors.toList())
         );
 
-        // 🌌 Realm dynamic
         manager.getCommandCompletions().registerCompletion("realms",
                 c -> realmManager.getAllRealms()
                         .stream()
