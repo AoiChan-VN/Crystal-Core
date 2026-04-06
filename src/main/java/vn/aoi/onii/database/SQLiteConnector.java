31da14276a8d6f70cec70a83fa7889dee7cbcb09
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-06 09:48:10 +0700

    Add files via upload

---
+++ src/main/java/vn/aoi/onii/database/SQLiteConnector.java
@@ -0,0 +1,42 @@
+package vn.aoi.onii.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SQLiteConnector {
+
+    private final HikariDataSource dataSource;
+
+    public SQLiteConnector(File dataFolder) {
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
+
+        File dbFile = new File(dataFolder, "aoi.db");
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:sqlite:" + dbFile.getAbsolutePath());
+        config.setMaximumPoolSize(10);
+        config.setPoolName("Aoi-Hikari");
+
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        this.dataSource = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public void shutdown() {
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
+    }
+} 
