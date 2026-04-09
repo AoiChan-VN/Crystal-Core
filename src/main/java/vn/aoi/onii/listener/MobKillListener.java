984f3ba53baaa071d5391b01e73a1d6fba6d4080
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-09 16:13:30 +0700

    Update MobKillListener.java

--- src/main/java/vn/aoi/onii/listener/MobKillListener.java
+++ src/main/java/vn/aoi/onii/listener/MobKillListener.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-import vn.aoi.onii.manager.CultivationService;
+import vn.aoi.onii.service.CultivationService;
 
 public class MobKillListener implements Listener {
 
