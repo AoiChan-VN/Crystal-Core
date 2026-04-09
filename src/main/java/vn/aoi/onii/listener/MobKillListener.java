1b69b623f19fc14a87f188058b17191c6ac1404d
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-04-10 00:20:23 +0700

    Update MobKillListener.java

--- src/main/java/vn/aoi/onii/listener/MobKillListener.java
+++ src/main/java/vn/aoi/onii/listener/MobKillListener.java
@@ -1,25 +1,30 @@
 package vn.aoi.onii.listener;
 
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-import vn.aoi.onii.service.CultivationService;
+import vn.aoi.onii.config.ConfigManager;
+import vn.aoi.onii.manager.CultivationService;
+import vn.aoi.onii.manager.MobManager;
 
 public class MobKillListener implements Listener {
 
+    private final MobManager mobManager;
     private final CultivationService cultivationService;
-    private final FileConfiguration config;
+    private final ConfigManager config;
 
-    public MobKillListener(CultivationService cultivationService, FileConfiguration config) {
+    public MobKillListener(MobManager mobManager,
+                           CultivationService cultivationService,
+                           ConfigManager config) {
+        this.mobManager = mobManager;
         this.cultivationService = cultivationService;
         this.config = config;
     }
 
     @EventHandler
     public void onKill(EntityDeathEvent event) {
+
         if (!(event.getEntity().getKiller() instanceof Player player)) return;
 
         double exp = mobManager.getExp(event.getEntityType());
